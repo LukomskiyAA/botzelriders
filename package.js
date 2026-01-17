818eb9bc7b87d936e1be973209f025ce95089a63
LukomskiyAA <lukomskii.aa@gmail.com>
2026-01-17 21:14:20 +0300

    Add files via upload

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "zel-riders-bot",
+  "version": "1.0.0",
+  "description": "Telegram bot for Zel Riders",
+  "main": "bot.js",
+  "scripts": {
+    "start": "node bot.js"
+  },
+  "dependencies": {
+    "grammy": "latest"
+  },
+  "engines": {
+    "node": ">=18.0.0"
+  }
+}
