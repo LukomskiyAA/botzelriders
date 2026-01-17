694ed258fafdeb700d38528b47bef812deb388e9
LukomskiyAA <lukomskii.aa@gmail.com>
2026-01-17 21:54:29 +0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,14 +1,12 @@
 const { Bot, InlineKeyboard } = require("grammy");
 const http = require("http");
 
-/**
- * Бот для Zel Riders
- * Токен: 8394525518:AAF5RD0yvNLZQjiTS3wN61cC3K2HbNwJtxg
- */
+// Токен бота: 8394525518:AAF5RD0yvNLZQjiTS3wN61cC3K2HbNwJtxg
 const bot = new Bot("8394525518:AAF5RD0yvNLZQjiTS3wN61cC3K2HbNwJtxg");
 
 bot.command("start", async (ctx) => {
-  const keyboard = new InlineKeyboard().url(
+  // Кнопка Mini App (webApp) вместо обычной ссылки (url)
+  const keyboard = new InlineKeyboard().webApp(
     "Заполнить анкету",
     "https://zel-riders-miniapp.vercel.app/"
   );
@@ -16,7 +14,7 @@ bot.command("start", async (ctx) => {
   await ctx.replyWithPhoto(
     "https://i.ibb.co/6RL2S1rv/nakleika.png",
     {
-      caption: `Zel RIders приветствует тебя!
+      caption: `Zel Riders приветствует тебя!
 
 Для дальнейшего вступления в чат, необходимо перейти по кнопке ниже и заполнить анкету!`,
       reply_markup: keyboard,
@@ -26,11 +24,9 @@ bot.command("start", async (ctx) => {
 
 bot.start();
 
-// Порт для Koyeb/Render (обязательно для прохождения билда)
+// Health check для Koyeb (обязателен)
 const PORT = process.env.PORT || 8080;
 http.createServer((req, res) => {
   res.writeHead(200);
-  res.end("Bot is alive!");
-}).listen(PORT, "0.0.0.0", () => {
-  console.log("Сервер проверки запущен на порту " + PORT);
-});
+  res.end("Bot is running");
+}).listen(PORT, "0.0.0.0");
